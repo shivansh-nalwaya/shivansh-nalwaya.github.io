477098322d2761fa57501c55d86a703687acc066
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2021-12-16 23:36:18 +0530

    Add castles

--- src/models/building.js
+++ src/models/building.js
@@ -1,26 +1,49 @@
+import * as THREE from "three";
 import Loaders from "../loaders";
 import scene from "../scene";
 
-const Building = 2; //= await Loaders.GLTFLoader.loadAsync("/assets/models/building/scene.gltf");
+const Building = await Loaders.GLTFLoader.loadAsync("/assets/models/castle-1/scene.gltf");
 
-// Building.scene.scale.setScalar(6);
-// Building.scene.rotation.set(0, -Math.PI / 2, 0);
-// Building.scene.position.set(0, 0, 13);
-// Building.scene.traverse((c) => {
-//   c.castShadow = true;
-// });
+Building.scene.scale.setScalar(4);
+Building.scene.rotation.set(0, Math.PI, 0);
+Building.scene.position.set(0, 0, -20);
+Building.scene.traverse((c) => {
+  c.castShadow = true;
+});
 
-// // scene.add(Building.scene);
+scene.add(Building.scene);
 
-// const Building2 = await Loaders.GLTFLoader.loadAsync("/assets/models/candy_shop/scene.gltf");
+const Building2 = await Loaders.GLTFLoader.loadAsync("/assets/models/tower/scene.gltf");
 
-// Building2.scene.scale.setScalar(5);
-// Building2.scene.rotation.set(0, -Math.PI / 2, 0);
-// Building2.scene.position.set(35, 0, 18);
-// Building2.scene.traverse((c) => {
-//   c.castShadow = true;
-// });
+Building2.scene.scale.setScalar(2);
+Building2.scene.rotation.set(0, -Math.PI / 2, 0);
+Building2.scene.position.set(-60, -2, 18);
+Building2.scene.traverse((c) => {
+  c.castShadow = true;
+});
 
-// scene.add(Building2.scene);
+scene.add(Building2.scene);
+
+const Building3 = await Loaders.GLTFLoader.loadAsync("/assets/models/tower-2/scene.gltf");
+
+Building3.scene.scale.setScalar(0.02);
+Building3.scene.rotation.set(0, -Math.PI / 2, 0);
+Building3.scene.position.set(60, 0, 18);
+Building3.scene.traverse((c) => {
+  c.castShadow = true;
+});
+
+scene.add(Building3.scene);
+
+const Building4 = await Loaders.GLTFLoader.loadAsync("/assets/models/warehouse/scene.gltf");
+
+Building4.scene.scale.setScalar(20);
+// Building4.scene.rotation.set(0, -Math.PI / 2, 0);
+Building4.scene.position.set(-80, 20, 50);
+Building4.scene.traverse((c) => {
+  c.castShadow = true;
+});
+
+scene.add(Building4.scene);
 
 export default Building;
