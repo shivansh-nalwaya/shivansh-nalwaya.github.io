15884fb2264dc80e641c92f8f2cad2205721bc0c
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2022-01-02 00:52:11 +0530

    Add raycaster for collision detection

--- src/animate.js
+++ src/animate.js
@@ -1,60 +1,54 @@
-import { Vector3 } from "three";
-import { Vec3 } from "cannon-es";
+import { Vector3, Raycaster } from "three";
 import CharacterIdle from "./animations/character-idle";
 import CharacterWalk from "./animations/character-walk";
 import camera from "./camera";
-import world, { cannonDebugRenderer } from "./physics";
-import Character, { characterBody } from "./models/character";
+import Character from "./models/character";
+import Mountain from "./models/mountain";
 import renderer from "./renderer";
 import scene from "./scene";
-import isDev from "./utils/is-dev";
 
 let previousRAF = 0,
   tempVector = new Vector3();
+var ray = new Raycaster();
+var rayPos = new Vector3();
 
 const animate = () => {
   requestAnimationFrame((t) => {
+    animate();
+    rayPos.set(Character.position.x, 100, Character.position.z);
+    var rayDir = new Vector3(0, -1, 0);
+
+    ray.set(rayPos, rayDir);
+
+    let intersect = ray.intersectObject(Mountain.scene);
+    if (Object.keys(intersect).length > 0) {
+      const y = intersect[0].point.y < 0 ? 0 : intersect[0].point.y;
+      Character.position.setY(y);
+    }
     if (Character.walkForward) {
-      characterBody.velocity.x = characterBody.quaternion.vmult(new Vec3(0, 0, 3)).x;
-      characterBody.velocity.z = characterBody.quaternion.vmult(new Vec3(0, 0, 3)).z;
+      Character.translateZ(0.4);
     }
     if (Character.turnLeft) {
       Character.rotation.y += 0.05;
-      characterBody.quaternion.copy(Character.quaternion);
-      if (!Character.walkForward) characterBody.velocity = characterBody.quaternion.vmult(new Vec3(0, 0, 3));
+      if (!Character.walkBackward && !Character.walkForward) Character.translateZ(0.4);
     }
     if (Character.turnRight) {
       Character.rotation.y -= 0.05;
-      characterBody.quaternion.copy(Character.quaternion);
-      if (!Character.walkForward) characterBody.velocity = characterBody.quaternion.vmult(new Vec3(0, 0, 3));
+      if (!Character.walkBackward && !Character.walkForward) Character.translateZ(0.4);
     }
     if (!Character.walkForward && !Character.turnLeft && !Character.turnRight) {
       CharacterIdle.update((t - previousRAF) * 0.001);
     } else {
       CharacterWalk.update((t - previousRAF) * 0.001);
-      camera.position.lerp(
-        new Vector3(Character.position.x - Math.sin(Character.rotation.y) * 20, Character.position.y + 10, Character.position.z - Math.cos(Character.rotation.y) * 20),
-        0.1
-      );
-      tempVector.copy(Character.position).y += 10;
+      camera.position.copy(Character.position);
+      camera.position.x -= Math.sin(Character.rotation.y) * 20;
+      camera.position.z -= Math.cos(Character.rotation.y) * 20;
+      camera.position.y += 10; // optional
+      tempVector.copy(Character.position).y += 10; // the += is optional
       camera.lookAt(tempVector);
     }
-    Character.position.copy(characterBody.position);
-    Character.position.y -= 5.5;
 
-    // const vertices = flag.geometry.attributes.position.array;
-    // for (let i = 0; i < vertices.length; i += 3) {
-    //   const x = i,
-    //     y = i + 1,
-    //     z = i + 2;
-    //   flag.geometry.attributes.position.array[z] = Math.sin(0.1 * vertices[x] + 0.1 * vertices[y] - t * ((vertices[x] + 7.5) / 15) * ((vertices[y] + 5) / 10) * 0.005);
-    // }
-    // flag.geometry.attributes.position.needsUpdate = true;
-
-    world.step(1.0 / 60.0, (t - previousRAF) * 0.003);
-    if (isDev) cannonDebugRenderer.update();
     previousRAF = t;
-    animate();
   });
   renderer.render(scene, camera);
 };
--- src/models/character.js
+++ src/models/character.js
@@ -7,17 +7,17 @@ const Character = await Loaders.FBXLoader.loadAsync("/assets/models/character.fb
 
 Character.scale.setScalar(0.07);
 Character.rotation.set(0, Math.PI, 0);
-Character.position.set(0, 5.5, 60);
+Character.position.set(0, 0, 60);
 Character.traverse((c) => {
   c.castShadow = true;
 });
 
 scene.add(Character);
 
-const shape = new Box(new Vec3(2, 5.5, 2));
-export const characterBody = new Body({ mass: 60, shape, angularDamping: 1 });
-characterBody.position.copy(Character.position);
-characterBody.quaternion.copy(Character.quaternion);
-world.addBody(characterBody);
+// const shape = new Box(new Vec3(2, 5.5, 2));
+// export const characterBody = new Body({ mass: 60, shape, angularDamping: 1 });
+// characterBody.position.copy(Character.position);
+// characterBody.quaternion.copy(Character.quaternion);
+// world.addBody(characterBody);
 
 export default Character;
