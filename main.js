b3578977fb37a23cfa099d03089129488b77739c
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2022-01-10 00:30:13 +0530

    Add ground

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let tempVector = new THREE.Vector3();
 class MainScene extends Scene3D {
   constructor() {
     super("MainScene");
-    this.speed = 3;
+    this.speed = 8;
   }
 
   async init() {
@@ -13,12 +13,13 @@ class MainScene extends Scene3D {
   }
 
   async preload() {
+    const untitled = this.load.preload("untitled", "/assets/untitled.gltf");
     const gallary = this.load.preload("gallary", "/assets/gallary.gltf");
     const character = this.load.preload("character", "/assets/character.fbx");
     const idle = this.load.preload("idle", "/assets/idle.fbx");
     const walk = this.load.preload("walk", "/assets/walk.fbx");
 
-    await Promise.all([character, idle, walk, gallary]);
+    await Promise.all([character, idle, walk, gallary, untitled]);
   }
 
   async create() {
@@ -36,29 +37,43 @@ class MainScene extends Scene3D {
     lights.directionalLight.shadow.camera.top = 250;
     lights.directionalLight.shadow.camera.bottom = -250;
 
-    this.ground = this.add.plane({ height: 200, width: 200 }, { lambert: { color: 0x00ff00 } });
-    this.ground.rotation.x = Math.PI / 2;
-    this.physics.add.existing(this.ground, {
-      shape: "plane",
-      mass: 0,
-      collisionFlags: 1,
-    });
-
     this.physics.debug.enable();
 
     this.camera.position.set(0, 3, 13);
+
+    const untitled = (await this.load.gltf("untitled")).scene;
+    untitled.scale.setScalar(10);
+    this.untitled = new ExtendedObject3D();
+    this.untitled.name = "untitled";
+    this.untitled.add(untitled);
+    this.untitled.rotation.set(0, 0, 0);
+    this.untitled.position.set(0, 0, 0);
+    this.untitled.traverse((child) => {
+      if (child.isMesh) {
+        child.castShadow = true;
+      }
+    });
+    this.add.existing(this.untitled);
+    this.physics.add.existing(this.untitled, {
+      shape: "box",
+      width: 100,
+      depth: 100,
+      height: 0,
+      mass: 0,
+      collisionFlags: 1,
+    });
 
     const gallary = (await this.load.gltf("gallary")).scene;
     gallary.scale.setScalar(10);
     this.gallary = new ExtendedObject3D();
     this.gallary.name = "gallary";
     this.gallary.add(gallary);
-    this.gallary.rotation.set(0, 0, 0);
-    this.gallary.position.set(0, -0.1, 20);
+    this.gallary.rotation.set(0, Math.PI / 2, 0);
+    this.gallary.position.set(0, -0.1, 32);
     this.gallary.traverse((child) => {
       if (child.isMesh) {
         child.castShadow = child.receiveShadow = true;
-        child.material.color = new THREE.Color(0xffff00);
+        child.material.color = new THREE.Color(0xbbbb00);
       }
     });
     this.add.existing(this.gallary);
@@ -69,7 +84,8 @@ class MainScene extends Scene3D {
     });
 
     this.floor = this.add.cylinder({ height: 0.1, radiusBottom: 9.99, radiusTop: 9.99, radiusSegments: 10 }, { lambert: 0x00ff00 });
-    this.floor.position.set(0, 0, 20);
+    this.floor.position.set(0, 0, 32);
+    this.floor.rotation.set(0, Math.PI / 2, 0);
 
     const man = await this.load.fbx("character");
 
@@ -91,7 +107,7 @@ class MainScene extends Scene3D {
     this.man.name = "character";
     this.man.add(man);
     this.man.rotation.set(0, 0, 0);
-    this.man.position.set(0, 3, 0);
+    this.man.position.set(0, 0, 0);
     this.man.traverse((child) => {
       if (child.isMesh) {
         child.castShadow = child.receiveShadow = true;
