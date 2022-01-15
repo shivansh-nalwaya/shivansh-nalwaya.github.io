bdb40cb977c48b9aa8f1b1f70dc29dcd1d2ce8a3
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2022-01-16 00:01:23 +0530

    Add water in swimming pool

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ class MainScene extends Scene3D {
       if (child.isMesh) {
         child.castShadow = true;
         if (child.name == "Sand") {
-          console.log(child);
+          // console.log(child);
           child.material.map = loader.load("/assets/sand-map.png");
           child.material.normalMap = loader.load("/assets/sand-normal.png");
         }
@@ -105,6 +105,22 @@ class MainScene extends Scene3D {
     this.man.body.setAngularFactor(0, 0, 0);
     this.man.body.setCcdMotionThreshold(1e-7);
     this.man.body.setCcdSweptSphereRadius(0.25);
+
+    // const textures = await Promise.all([this.load.texture("/assets/Water_1_M_Normal.jpeg"), this.load.texture("/assets/Water_2_M_Normal.jpeg")]);
+
+    // textures[0].needsUpdate = true;
+    // textures[1].needsUpdate = true;
+
+    // this.misc.water({
+    //   x: -53,
+    //   z: 0,
+    //   y: -0.2,
+    //   height: 12,
+    //   normalMap0: textures[0],
+    //   normalMap1: textures[1],
+    //   flowX: 0,
+    //   flowY: 0,
+    // });
 
     this.keys = {
       up: { isDown: false },
